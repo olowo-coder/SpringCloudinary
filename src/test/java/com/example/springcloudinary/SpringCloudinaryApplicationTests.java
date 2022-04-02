abc35facab3218d6b52a7adb4041f0123cf0fa9d
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-04-02 22:30:07 +0100

    Initialization

---
+++ src/test/java/com/example/springcloudinary/SpringCloudinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudinaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
